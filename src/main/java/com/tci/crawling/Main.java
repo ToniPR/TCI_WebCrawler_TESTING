5081fef7acf6efcfb6b9b32ca34701bc77ca1dc7
Toni Pinto Rebelo <t.rebelo@student.fontys.nl>
2018-06-03 15:15:57 +0200

    first commit

---
+++ src/main/java/com/tci/crawling/Main.java
@@ -0,0 +1,20 @@
+package com.tci.crawling;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Crawler crawler = new Crawler("http://localhost/WebCrawler/Webcrawler/src/main/webapp/sample_site_to_crawl/");
+
+        System.out.print("################################## Web Crawling with ID ##########################################\n");
+
+        System.out.print(crawler.searchAlgorithmWithIdDFS(102) + " \n");
+
+        System.out.print("################################## Web Crawling with search word ##################################\n");
+
+        System.out.print(crawler.searchAlgorithmWithWordDFS("No Fences"));
+
+    }
+}
